c155f71fee4f073ff8afafc4acfb6ef4a522d4dd
DawalaGH <dawalaamidudar@gmail.com>
2025-01-26 16:47:09 +0000

    first commit

---
+++ Reverse a string.py
@@ -0,0 +1,6 @@
+word=input("please enter a word or sentence:")
+word2=("")
+for i in word:
+ word2=i+word2
+print("\nOriginal word:",word)
+print("Reversed word:",word2)
---
+++ Sum of whole numbers.py
@@ -0,0 +1,6 @@
+#take input from user
+n=int(input("enter value to find sum: "))
+sum=10
+for i in range(1,n+1):
+ sum=sum+1
+ print("\nsum=",sum)
